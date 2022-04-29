9b6fc6bc6a4eec3d1247d2f3c1a2c9bbd205216c
jakovid <jacobk.dowdle@gmail.com>
2022-04-29 10:45:54 +0800

    simplified variable selection with arrays, added dom manipulation of emojis

--- script.js
+++ script.js
@@ -3,38 +3,34 @@ let playerSelection;
 let computerSelection;
 let playerScore = 0;
 let computerScore = 0;
+const choices = ['rock', 'paper', 'scissors'];
+const rpsEmoji = {rock:'&#9994', paper:'&#9995', scissors:'&#9996'};
 
 /* event listeners */
 
 
 /* functions */
 
-/* logs the computer's choice */
+/* sets the computer's choice */
 function computerPlay() {
     let randomNum = Math.floor(Math.random() * 3);
-    switch (randomNum) {
-        case 0:
-            return "rock";
-        case 1:
-            return "paper";
-        case 2:
-            return "scissors";
-    }
+    return choices[randomNum];
 }
 
+/* sets player variable, plays a round of the game, and updates DOM*/
 function playGame(playerInput) {
-    if (playerInput == "rock") {
-        playerSelection = "rock";
-    } else if (playerInput == "paper") {
-        playerSelection = "paper";
-    } else if (playerInput == "scissors") {
-        playerSelection = "scissors";
-    }
+    playerSelection = choices[playerInput];
     computerSelection = computerPlay();
     playRound(playerSelection, computerSelection);
+    console.log(`You: ${playerScore} Computer: ${computerScore}`);
+    document.getElementById("playerScore").innerHTML = `You: ${playerScore}`;
+    document.getElementById("playerChoice").innerHTML = `${rpsEmoji[playerSelection]}`;
+    document.getElementById("computerScore").innerHTML = `Computer: ${computerScore}`;
+    document.getElementById("computerChoice").innerHTML = `${rpsEmoji[computerSelection]}`;
+
 }
 
-/* runs a round of the game */
+/* runs a round of the game*/
 function playRound(playerSelection, computerSelection) {
     if(playerSelection == "rock" && computerSelection == "scissors" 
     || playerSelection == "paper" && computerSelection == "rock"
@@ -53,7 +49,4 @@ function playRound(playerSelection, computerSelection) {
         console.log(`you lose! ${computerSelection} beats ${playerSelection}!`);
         computerScore++;
     }
-    console.log(`You: ${playerScore} Computer: ${computerScore}`);
-    document.getElementById("playerScore").innerHTML = `Player Score: ${playerScore}`;
-    document.getElementById("computerScore").innerHTML = `Computer Score: ${computerScore}`;
 }
